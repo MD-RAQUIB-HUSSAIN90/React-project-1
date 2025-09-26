e96867bf682df894f4c3d048a56343bdf32a8cb2
MD RAQUIB HUSSAIN <mdraquibhussain71@gmail.com>
2025-09-26 23:58:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+          <ul>
+          <li href="#">Menu</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+          </ul>
+          <button>Login</button>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
